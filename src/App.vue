f3f28cb62bd53ebbed3060585e896d8b1f51f21a
aronyu <h0935674938@hotmail.com>
2020-06-08 22:39:52 +0800

    Add login sign_out feature

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" />
-    <router-view />
-    <button type="button" class="btn btn-primary">Primary</button>
-    <button type="button" class="btn btn-secondary">Secondary</button>
-    <button type="button" class="btn btn-success">Success</button>
-    <button type="button" class="btn btn-danger">Danger</button>
-    <button type="button" class="btn btn-warning">Warning</button>
-    <button type="button" class="btn btn-info">Info</button>
-    <button type="button" class="btn btn-light">Light</button>
-    <button type="button" class="btn btn-dark">Dark</button>
-
-    <button type="button" class="btn btn-link">Link</button>
+    <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: "App",
-  created() {
-    const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products/all`;
-    this.$http.get(api).then(response => {
+  name: 'App',
+  created(){
+    const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products/all`
+    this.$http.get(api).then((response)=> {
       console.log(response.data);
-    });
-  }
-};
+    })
+  },
+}
 </script>
 
 <style lang='scss'>
