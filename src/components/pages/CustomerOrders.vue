33fc3bcf3f6fcc1417363d45efd3117bb6cca4a2
aronyu <h0935674938@hotmail.com>
2020-06-24 00:05:40 +0800

    Add customer_order page product model

--- src/components/pages/CustomerOrders.vue
+++ src/components/pages/CustomerOrders.vue
@@ -2,9 +2,12 @@
   <div>
     <loading :active.sync="isLoading"></loading>
     <div class="row mt-4">
-      <div class="col-md-4 mb-4" v-for='item in products' :key='item.id'>
+      <div class="col-md-4 mb-4" v-for="item in products" :key="item.id">
         <div class="card border-0 shadow-sm">
-          <div style="height: 150px; background-size: cover; background-position: center" :style='{backgroundImage: `url(${item.image})`}'></div>
+          <div
+            style="height: 150px; background-size: cover; background-position: center"
+            :style="{backgroundImage: `url(${item.image})`}"
+          ></div>
           <div class="card-body">
             <span class="badge badge-secondary float-right ml-2">{{ item.category }}</span>
             <h5 class="card-title">
@@ -18,12 +21,61 @@
             </div>
           </div>
           <div class="card-footer d-flex">
-            <button type="button" class="btn btn-outline-secondary btn-sm">
-              <i class="fas fa-spinner fa-spin"></i>
+            <button
+              type="button"
+              class="btn btn-outline-secondary btn-sm"
+              @click.prevent="getProduct(item.id)"
+            >
+              <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id "></i>
               查看更多
             </button>
             <button type="button" class="btn btn-outline-danger btn-sm ml-auto">
-              <i class="fas fa-spinner fa-spin"></i>
+              <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
+              加到購物車
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="modal fade"
+      id="productModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content border-0">
+          <div class="modal-header bg-dark text-white">
+            <h5 class="modal-title" id="exampleModalLabel">{{ product.title }}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <img :src="product.image" class="img-fluid" alt />
+            <blockquote class="blockquote mt-3">
+              <p class="mb-0">{{ product.content }}</p>
+              <footer class="blockquote-footer text-right">{{ product.description }}</footer>
+            </blockquote>
+            <div class="d-flex justify-content-between align-items-baseline">
+              <div class="h5" v-if="!product.price">{{ product.origin_price }}</div>
+              <del class="h6" v-if="product.price">原價 {{ product.origin_price }}</del>
+              <div class="h5" v-if="product.price">現在只要 {{ product.price }}</div>
+            </div>
+            <select name="" class="form-control mt-3" v-model="product.num">
+              <option value="1">選購一件</option>
+            </select>
+          </div>
+          <div class="modal-footer">
+            <div class="text-muted text-nowrap mr-3">
+              小計
+              <strong>{{ product.num * product.price }}</strong>元
+            </div>
+            <button type="button" class="btn btn-primary">
+              <!-- <i class="fas fa-spinner fa-spin" v-if="product.id === status.loadingItem"></i> -->
               加到購物車
             </button>
           </div>
@@ -34,10 +86,16 @@
 </template>
 
 <script>
+import $ from "jquery";
+
 export default {
   data() {
     return {
       products: [],
+      product: {},
+      status: {
+        loadingItem: ""
+      },
       isLoading: false
     };
   },
@@ -48,8 +106,19 @@ export default {
       vm.isLoading = true;
       this.$http.get(api).then(response => {
         vm.products = response.data.products;
-        vm.pagination = response.data.pagination;
+        // vm.pagination = response.data.pagination;
         vm.isLoading = false;
+      });
+    },
+    getProduct(id) {
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/product/${id}`;
+      const vm = this;
+      vm.status.loadingItem = id;
+      this.$http.get(api).then(response => {
+        console.log(response);
+        vm.product = response.data.product;
+        $("#productModal").modal("show");
+        vm.status.loadingItem = "";
       });
     }
   },
